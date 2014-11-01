233f1537828f63cafe8e492eeebf32067cbb3338
Adjective-Object <mhuan13@gmail.com>
2014-10-31 20:56:22 -0400

    git good skrub

---
+++ ocean-client.py
@@ -0,0 +1,19 @@
+from flask import Flask
+app = Flask(__name__)
+
+state=Object()
+state.currentString = ""
+
+
+@app.route("/char")
+def hello():
+
+    return "fuck"
+
+@app.route("/")
+def hello():
+    return "fuck"
+
+
+if __name__ == "__main__":
+    app.run()
